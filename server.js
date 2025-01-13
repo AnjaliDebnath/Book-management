cb58daa1f249d1e071db5d00b0bbb4890376e8ab
AnjaliDebnath <anjalidebnath1003@gmail.com>
2025-01-13 15:23:11 +0530

    updated server

--- server.js
+++ server.js
@@ -12,5 +12,16 @@ app.use(express.json());
 const bookRoutes= require('./routes/bookRoutes');
 app.use('/books', bookRoutes);
 
+const userRoutes = require('./routes/userRoutes');
+app.use('/users', userRoutes);
+
+const authorRoutes = require('./routes/authorRoutes');
+app.use('/authors', authorRoutes);
+
+const borrowedBooksRoutes = require('./routes/borrowedBooksRoutes');
+app.use('/borrow', borrowedBooksRoutes);
+
+
+
 
 app.listen(process.env.PORT)
